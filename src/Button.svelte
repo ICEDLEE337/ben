fbb8b6ed97457e03dcff2612c8019e84184828f4
Lee Norris <lee.norris@arcadia.io>
2019-10-25 13:10:23 -0400

    first

---
+++ src/Button.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let text;
+</script>
+
+<svelte:options tag="arc-btn" />
+<button on:click>{text}</button>
+
+<style type="text/scss">
+$color: hotpink;
+
+button {
+    background-color: $color;
+}
+</style>
